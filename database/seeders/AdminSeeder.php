39743a6f18be9c20c974e1a218f872e4e04a6d5c
Shinsaku-Noto <shinsaku32823@gmail.com>
2024-05-27 16:35:35 +0800

    added seeder

--- database/seeders/AdminSeeder.php
+++ database/seeders/AdminSeeder.php
@@ -5,7 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\User;
-use Illuminate\Database\Facades\Hash;
+use Illuminate\Support\Facades\Hash;
 
 class AdminSeeder extends Seeder
 {
